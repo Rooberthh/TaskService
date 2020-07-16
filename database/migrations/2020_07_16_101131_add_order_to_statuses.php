e6f783b36ab6a27315d4eba35ba3eb1ffdf2566a
Rooberthh <roberth.evaldsson@hotmail.com>
2020-07-16 12:15:54 +0200

    add proper migration to add order column

--- database/migrations/2020_07_16_101131_add_order_to_statuses.php
+++ database/migrations/2020_07_16_101131_add_order_to_statuses.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateStatusesTable extends Migration
+class AddOrderToStatuses extends Migration
 {
     /**
      * Run the migrations.
@@ -13,12 +13,8 @@ class CreateStatusesTable extends Migration
      */
     public function up()
     {
-        Schema::create('statuses', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->unsignedInteger('board_id');
-            $table->string('name');
-            $table->string('color')->default('#000000');
-            $table->timestamps();
+        Schema::table('statuses', function (Blueprint $table) {
+            $table->unsignedInteger('order')->default(1000)->after("color");
         });
     }
 
@@ -29,6 +25,8 @@ class CreateStatusesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('statuses');
+        Schema::table('statuses', function (Blueprint $table) {
+            $table->dropColumn(['order']);
+        });
     }
 }
